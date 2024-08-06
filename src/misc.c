8c4804352b74b8658926bdb2d1a6b43a5d29b9e5
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-08-06 10:17:12 -0600

    fix: Misc `misc.c` corrections.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- src/misc.c
+++ src/misc.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
 #include <jeff/jeff.h>
 
 char **filter_argv(const int argc, char **argv) {
@@ -9,7 +10,7 @@ char **filter_argv(const int argc, char **argv) {
 
   char **result = ALLOC(char*, argc - 1);
 
-  for (unsigned int i = 1; i < (unsigned int)argc; i++) {
+  for (unsigned int i = 1; i < (uint)argc; i++) {
     result[i - 1] = argv[i];
   }
 
@@ -17,14 +18,13 @@ char **filter_argv(const int argc, char **argv) {
 }
 
 int main(int argc, char **argv) {
-
   char **args = filter_argv(argc, argv);
 
   if (args == NULL) {
-    die(1, "No arguments given.");
+    die(1, "No arguments given");
   }
 
-  for (unsigned int i = 0; i < (unsigned int)argc - 1; i++) {
+  for (uint i = 0; i < (uint)argc - 1; i++) {
     printf("%s\n", args[i]);
   }
 
