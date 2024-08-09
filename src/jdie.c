58587dba69b852d9161c24cd006db742bce1860e
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-08-09 13:44:13 -0600

    chore(jdie): Add comments.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- src/jdie.c
+++ src/jdie.c
@@ -3,9 +3,9 @@
 #include <jeff/jeff.h>
 
 void die(const int status, const char *msg) {
-  if (msg != NULL && status) {
+  if (msg != NULL && status) { /// If status is not 0 and message is available
     err(msg);
-  } else if (msg != NULL) {
+  } else if (msg != NULL) { /// If status is 0 and message is available
     printf("%s\n", msg);
   }
 
