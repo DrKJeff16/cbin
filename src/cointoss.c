50ad4c2ae828452a7d6fd821cde66b9cee0f823b
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2024-08-16 18:42:14 -0600

    style(cointoss): Indented file.
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- src/cointoss.c
+++ src/cointoss.c
@@ -8,88 +8,88 @@
 #include <jeff/cointoss.h>
 
 static void seed(jbool *seeded) {
-    if (!(*seeded)) {
-        srand(time(NULL));
-        *seeded = 1;
-    }
+  if (!(*seeded)) {
+    srand(time(NULL));
+    *seeded = 1;
+  }
 }
 
 static void decide(const uint x, CHOICES *c) {
-    switch(x) {
-        case 1:
-            c->HEADS++;
-            break;
-        case 0:
-        default:
-            c->TAILS++;
-            break;
-    }
+  switch(x) {
+    case 1:
+      c->HEADS++;
+      break;
+    case 0:
+    default:
+      c->TAILS++;
+      break;
+  }
 }
 
 static void final_decide(CHOICES *c, char *tails_msg, char *heads_msg) {
-    if (c->HEADS > c->TAILS) {
-        printf("%s\n", tails_msg);
-    } else if (c->TAILS > c->HEADS) {
-        printf("%s\n", heads_msg);
-    } else {
-        char **msgs = CALLOC(char*, 2);
-        char **og = msgs;
+  if (c->HEADS > c->TAILS) {
+    printf("%s\n", tails_msg);
+  } else if (c->TAILS > c->HEADS) {
+    printf("%s\n", heads_msg);
+  } else {
+    char **msgs = CALLOC(char*, 2);
+    char **og = msgs;
 
-        *msgs = tails_msg;
-        ++msgs;
-        *msgs = heads_msg;
-        msgs = og;
+    *msgs = tails_msg;
+    ++msgs;
+    *msgs = heads_msg;
+    msgs = og;
 
-        printf("%s\n", msgs[toss()]);
+    printf("%s\n", msgs[toss()]);
 
-        free(msgs);
-    }
+    free(msgs);
+  }
 }
 
 static char *error_msg(void) {
-    char *result = "Need two arguments";
+  char *result = "Need two arguments";
 
-    return result;
+  return result;
 }
 
 uint toss(void) {
-    return (uint)(rand() % 2);
+  return (uint)(rand() % 2);
 }
 
 int main(int argc, char **argv) {
-    argc--;
+  argc--;
 
-    if (argc != 2) {
-        die(127, error_msg());
-    }
+  if (argc != 2) {
+    die(127, error_msg());
+  }
 
-    jbool *seeded = CALLOC(jbool, 1);
-    *seeded = 0;
+  jbool *seeded = MALLOC(jbool);
+  *seeded = JFALSE;
 
-    seed(seeded);
+  seed(seeded);
 
-    char *tails_msg, *heads_msg;
-    char **argv_og = argv;
+  char *tails_msg, *heads_msg;
+  char **argv_og = argv;
 
-    (++argv);
-    tails_msg = *argv;
-    (++argv);
-    heads_msg = *argv;
+  (++argv);
+  tails_msg = *argv;
+  (++argv);
+  heads_msg = *argv;
 
-    argv = argv_og;
+  argv = argv_og;
 
-    CHOICES *c = MALLOC(CHOICES);
-    c->TAILS = 0;
-    c->HEADS = 0;
+  CHOICES *c = MALLOC(CHOICES);
+  c->TAILS = 0;
+  c->HEADS = 0;
 
-    for (unsigned long long i = 0L; i < 100000L; i++ ){
-        decide(toss(), c);
-    }
+  for (unsigned long long i = 0L; i < 100000L; i++ ){
+    decide(toss(), c);
+  }
 
-    final_decide(c, tails_msg, heads_msg);
+  final_decide(c, tails_msg, heads_msg);
 
-    free(c);
-    free(seeded);
+  free(c);
+  free(seeded);
 
-    return 0;
+  return 0;
 }
