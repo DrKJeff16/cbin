d2c546b123c4cd7eb277cea29276e8b552ef562a
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-11 12:42:56 -0600

    feat: actually built `yes_no`
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- src/yes_no.c
+++ src/yes_no.c
@@ -1,8 +1,7 @@
-#include <errno.h>
-#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 #include <jeff/jeff.h>
 #include <yes_no.h>
 
@@ -14,31 +13,63 @@ void free_valid(char **valid, const size_t argc) {
   free(valid);
 }
 
+void sig_handler(int sig) {
+  vdie(sig, "\nCaught signal %d\n", sig);
+}
+
 int main(int argc, char **argv) {
+  signal(SIGINT, sig_handler);
+  signal(SIGTERM, sig_handler);
+
   argc--;
 
   if (argc == 0) {
     j_err("%s\n", "No arguments provided!");
-    return 1;
+    return 127;
   }
 
-  size_t i = 0, len = (size_t)argc;
-  char **valid = CALLOC(char *, len);
+  // size_t i = 0, len = (size_t)argc;
+  // char **valid = CALLOC(char *, len);
 
-  for (; i < len; i++) {
-    size_t len = strlen(argv[i + 1]);
-    valid[i] = CALLOC(char, len + 1);
+  // for (; i < len; i++) {
+  //   size_t len = strlen(argv[i + 1]);
+  //   valid[i] = CALLOC(char, len + 1);
+  //
+  //   if (null_ptr(stpcpy(valid[i], argv[i + 1]))) {
+  //     free_valid(valid, len);
+  //     return 1;
+  //   }
+  //
+  //   printf("%zu: `%s`", i, valid[i]);
+  //   j_strip('-', valid[i]);
+  //   printf("  ===>  `%s`\n", valid[i]);
+  // }
 
-    if (null_ptr(stpcpy(valid[i], argv[i + 1]))) {
-      free_valid(valid, len);
-      return 1;
+  // free_valid(valid, len);
+
+  while (JTRUE) {
+    char input[4];
+    char *msg = CALLOC(char, strlen(argv[1]) + 1);
+    stpcpy(msg, argv[1]);
+    j_rstrip('?', msg);
+
+    printf("%s?[Y/N]: ", msg);
+    fgets(input, sizeof(input), stdin);
+    input[strcspn(input, "\n")] = 0;
+
+    switch (input[0]) {
+      case 'y':
+      case 'Y':
+        return 0;
+
+      case 'n':
+      case 'N':
+        return 1;
+
+      default:
+        break;
     }
-
-    printf("%zu: `%s`", i, valid[i]);
-    j_strip('-', valid[i]);
-    printf("  ===>  `%s`\n", valid[i]);
   }
 
-  free_valid(valid, len);
-  return 0;
+  return 127;
 }
