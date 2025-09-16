1fc7c7732504c8c0420b255b29b2bd63dff67119
Guennadi Maximov C <g.maxc.fox@protonmail.com>
2025-09-15 23:31:32 -0600

    chore: improved signaling
    Signed-off-by: Guennadi Maximov C <g.maxc.fox@protonmail.com>

--- src/jsignal.c
+++ src/jsignal.c
@@ -1,24 +1,28 @@
 #include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <jeff/jeff.h>
 
 void sig_handler(const int sig) {
   switch (sig) {
     case SIGINT:
       {
-        fprintf(stderr, "\nCtrl-C detected: %d\n", sig);
+        j_verr("\nCtrl-C detected: %d\n", sig);
+        break;
+      }
+
+    case SIGTERM:
+      {
+        j_verr("\nSIGTERM DETECTED: %d\n", sig);
         break;
       }
 
     default:
       {
-        fprintf(stderr, "\nCaught signal %d\n", sig);
+        j_verr("\nCaught signal %d\n", sig);
         break;
       }
   }
 
-  exit(sig);
+  die(sig, NULL);
 }
 
 /// vim:ts=2:sts=2:sw=2:et:ai:si:sta:noci:nopi:
