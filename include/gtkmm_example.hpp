2600721434e5d14bdbcf405106202c303886dd33
DrKJeff16 <72052712+DrKJeff16@users.noreply.github.com>
2026-03-02 08:26:15 +0000

    chore: format files with `vim-eof-comment`
    Signed-off-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- include/gtkmm_example.hpp
+++ include/gtkmm_example.hpp
@@ -6,3 +6,5 @@ class MyWindow: public Gtk::Window {
  public:
   MyWindow();
 };
+
+/* vim: set ts=2 sts=2 sw=2 et ai si sta: */
--- src/gtkmm_example.cpp
+++ src/gtkmm_example.cpp
@@ -11,3 +11,5 @@ int main(int argc, char *argv[]) {
   auto app = Gtk::Application::create("org.gtkmm.examples.base");
   return app->make_window_and_run<MyWindow>(argc, argv);
 }
+
+/* vim: set ts=2 sts=2 sw=2 et ai si sta: */
